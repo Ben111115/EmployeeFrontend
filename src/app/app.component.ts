269d27af4c20c0f1a3ce6cbce90ef74a88333de2
Ben <formhalsben@gmail.com>
2025-01-29 10:00:56 +0100

    Header and Usercard implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UsercardComponent } from "./usercard/usercard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UsercardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
